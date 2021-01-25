a3af687cef679d4d01d6e4e0fc182f8ed9f024fe
Jordan Goodwin <jordan.goodwin-2017@hull.ac.uk>
2021-01-25 15:17:12 +0000

    Initial commit

---
+++ Assets/Scripts/MainMenu/StartMenuGameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenuGameManager : MonoBehaviour
+{
+    public enum VRSelectables
+    {
+        NoVR,
+        OculusRift
+    }
+
+    [Header("Settings Menu")]
+    public GameObject optionsMenuHolder;
+    public VRSelectables vrSetting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vrSetting = VRSelectables.NoVR; // default vr off setting
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowSettingsMenu()
+    {
+        optionsMenuHolder.SetActive(true);
+    }
+
+    public void HideSettingsMenu()
+    {
+        optionsMenuHolder.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        switch (vrSetting)
+        {
+            case VRSelectables.NoVR:
+                //Load non vr scene
+                SceneManager.LoadScene(1);
+                break;
+            case VRSelectables.OculusRift:
+                //Load Oculus VR scene
+                Debug.Log("I got into here");
+                SceneManager.LoadScene(2);
+                break;
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void UpdateVRSetting(GameObject dropdown)
+    {
+        switch (dropdown.GetComponent<Dropdown>().value)
+        {
+            case 0:
+                vrSetting = VRSelectables.NoVR;
+                Debug.Log("Successfully selected NoVR");
+                break;
+            case 1:
+                vrSetting = VRSelectables.OculusRift;
+                Debug.Log("Successfully selected OculusRift");
+                break;
+            default:
+                Debug.Log("An error has occurred with selecting the VR setting");
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/NoVR/PlyController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlyController : MonoBehaviour
+{
+    public Transform target, pivot;
+    public float mouseSensitivity, playerSpeed, walkSpeed, sprintSpeed, jumpHeight;
+
+    private Vector3 moveDirection;
+    private float xRot, yRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeed = walkSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yRot += Input.GetAxis("Mouse X") * mouseSensitivity;
+        xRot += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        if(xRot > 90f)
+        {
+            xRot = 90f;
+        }else if (xRot < -90f)
+        {
+            xRot = -90f;
+        }
+        
+        transform.localEulerAngles = new Vector3(-xRot, yRot, transform.localEulerAngles.z);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                moveDirection = transform.forward * Input.GetAxis("Vertical") * sprintSpeed;
+                moveDirection.y = 0;
+            }
+            else
+            {
+                moveDirection = transform.forward * Input.GetAxis("Vertical") * walkSpeed;
+                moveDirection.y = 0;
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                moveDirection = transform.forward * Input.GetAxis("Vertical") * sprintSpeed;
+                moveDirection.y = 0;
+            }
+            else
+            {
+                moveDirection = transform.forward * Input.GetAxis("Vertical") * walkSpeed;
+                moveDirection.y = 0;
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                moveDirection = transform.right * Input.GetAxis("Horizontal") * sprintSpeed;
+                moveDirection.y = 0;
+            }
+            else
+            {
+                moveDirection = transform.right * Input.GetAxis("Horizontal") * walkSpeed;
+                moveDirection.y = 0;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                moveDirection = transform.right * Input.GetAxis("Horizontal") * sprintSpeed;
+                moveDirection.y = 0;
+            }
+            else
+            {
+                moveDirection = transform.right * Input.GetAxis("Horizontal") * walkSpeed;
+                moveDirection.y = 0;
+            }
+        }
+        if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            moveDirection = new Vector3(0, 0, 0);
+        }
+
+        transform.position = transform.position + (moveDirection * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("NoVRExitDoor"))
+        {
+            Application.Quit();
+        }
+        if (collider.CompareTag("NoVRShootingHouseDoor"))
+        {
+            SceneManager.LoadScene("NoVRShootingHouse");
+        }
+    }
+}
